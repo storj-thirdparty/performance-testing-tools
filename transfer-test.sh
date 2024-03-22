d98f85e39c2a07e5b6a08aa6c678ee73264e6ef8
karl <karl@utropicmedia.com>
2024-03-21 22:06:40 -0400

    Create transfer-test.sh
    initial commit

---
+++ transfer-test.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# vars
+TARGET_DIR=~
+ACCESS=
+AWS_ACCESS_KEY_ID=
+AWS_SECRET_ACCESS_KEY=
+AWS_ENDPOINT=
+PARALLEL=24
+
+
+
+# install tools
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+apt-get install -y zip bmon awscli curl speedtest
+curl -L https://github.com/storj/storj/releases/latest/download/uplink_linux_amd64.zip -o uplink_linux_amd64.zip
+unzip -o uplink_linux_amd64.zip
+sudo install uplink /usr/local/bin/uplink
+
+
+# config tools
+aws configure set default.s3.multipart_threshold 60MB
+aws configure set default.s3.multipart_chunksize 60MB
+aws configure set default.s3.max_concurrent_requests $PARALLEL
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+
+uplink access import default $ACCESS
+
+
+# make test data
+dd if=/dev/urandom of=testfile10G.bin iflag=fullblock bs=1G count=10
+dd if=/dev/urandom of=testfile50G.bin iflag=fullblock bs=1G count=50
+dd if=/dev/urandom of=testfile100G.bin iflag=fullblock bs=1G count=100
+
+# take baseline
+speedtest --accept-gdpr --accept-license
+
+### run tests
+## aws
+# upload
+time aws s3 cp testfile10G.bin s3://testbucket2 --endpoint-url=$AWS_ENDPOINT
+time aws s3 cp testfile50G.bin s3://testbucket2 --endpoint-url=$AWS_ENDPOINT
+time aws s3 cp testfile100G.bin s3://testbucket2 --endpoint-url=$AWS_ENDPOINT
+
+# download
+time aws s3 cp s3://testbucket2/testfile10G.bin $TARGET_DIR --endpoint-url=$AWS_ENDPOINT
+time aws s3 cp s3://testbucket2/testfile50G.bin $TARGET_DIR --endpoint-url=$AWS_ENDPOINT
+time aws s3 cp s3://testbucket2/testfile100G.bin $TARGET_DIR --endpoint-url=$AWS_ENDPOINT
+
+
+## cleanup storj
+uplink rm sj://testbucket2/testfile10G.bin
+uplink rm sj://testbucket2/testfile50G.bin
+uplink rm sj://testbucket2/testfile100G.bin
+
+
+
+## uplink
+# upload
+time uplink cp testfile10G.bin sj://testbucket2 -p $PARALLEL 
+time uplink cp testfile50G.bin sj://testbucket2 -p $PARALLEL 
+time uplink cp testfile100G.bin sj://testbucket2 -p $PARALLEL
+
+## cleanup storj
+rm -f testfile10G.bin
+rm -f testfile50G.bin
+rm -f testfile100G.bin
+
+
+# download
+time uplink cp sj://testbucket2/testfile10G.bin $TARGET_DIR -p $PARALLEL
+time uplink cp sj://testbucket2/testfile50G.bin $TARGET_DIR -p $PARALLEL
+time uplink cp sj://testbucket2/testfile100G.bin $TARGET_DIR -p $PARALLEL
